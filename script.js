6d67a24e56fe1c31d6447e2bbf2c79f734278fc0
MANISHA SHAW <143929772+MANISHA-SHAW@users.noreply.github.com>
2023-09-24 22:35:39 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let counter = 0
+function increment(){
+    // counter++;
+    counter = counter + 1;
+    //counter +=1;
+    document.getElementById('counter').innerHTML = counter;
+
+}
+function decrement(){
+    // counter--;
+    counter = counter - 1;
+    //counter -=1;
+    document.getElementById('counter').innerHTML = counter;
+
+}
+function reset(){
+   counter = 0;
+    document.getElementById('counter').innerHTML = counter;
+
+}
+
+
